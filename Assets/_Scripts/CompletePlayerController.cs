5f5636572450719fea8cfcb4f135507d5ac59217
oi86039 <39169568+oi86039@users.noreply.github.com>
2018-10-04 02:29:21 -0400

    10/4/18 - Lot's of progress with player polish and control! Smoothed Player Jump Smoothed Player Dash Learned the basics of Coroutines to make the Dash a Timed action Created a rough dash jump (could improve) Improved projectile script by deleting a duplicate prefab for more than one direction Fixed bug with wall jump that made it nearly nonfunctional Fixed bug with wall jump that increased gravity until the next dash Enlarged test level for more mobility capabilities Changed the wall jump to a coroutine based action. Changed jump speeds Assigned public variables to most, if not all, movement variables for easy adjustments Implemented drag
    Finishing this commit at about 2:30 in the morning. I'm definitely going to regret staying up so long, but I'm so glad about the progress I've made. It feels like megaman! Once I get the charge shot and saber, that's one of the harder deliverables done. The next hardest will be level design, then AI. But this is going well!
    
    My mood has been fluctuating a ton lately, but the C-CAPS center really helped me out. I felt stable for longer. I got a plan. And an appointment for tomorrow to check up on me. I feel good. The intrusive thoughts don't stop, but at least the intensity has gone down today. I'll write more about it in the morning; I need rest. -_-

--- Assets/_Scripts/CompletePlayerController.cs
+++ Assets/_Scripts/CompletePlayerController.cs
@@ -6,30 +6,37 @@ public class CompletePlayerController : MonoBehaviour
 	public float speed;
 	//Walk speed
 	public float jumpHeight;
-
+	public float wallJumpHeight;
+	public float wallJumpKnock;
+	public float wallJumpTime;
 	private float jumpTimeCounter;
 	public float jumpTime;
+	public bool isJumping;
+	public float gravity;
+	public float wallGravity;
+	public float drag;
+	public float terminalVel;
 
+	//public KeyCode dashCode;		//Store keypress
+	private float dashTimeCounter;
 	public float dashSpeed;
-	//public float dashTimer;	//Helps with temporary anti-gravity effect of the air dash
-
+	public float dashTime;
+	//Helps with temporary anti-gravity effect of the air dash
+	public bool isDashing;
 	public bool lastDir;
 	//Last direction. True = right, False = left, default = right
-
+	public bool canTurn;
 	public bool onGround;
 	public bool onLeftWall;
 	public bool onRightWall;
-	public bool isJumping;
-	//If holding up arrow.
 
 	private Rigidbody2D rb2d;
 	private SpriteRenderer sprite;
-	public GameObject lemonL;
-	public GameObject lemonR;
-   
+	public GameObject lemon;
 	// Use this for initialization
 	void Start ()
 	{
+		canTurn = true;
 		lastDir = true; //Default direction = right
 		onGround = true; //Default on the ground
 		onLeftWall = false;
@@ -37,82 +44,147 @@ public class CompletePlayerController : MonoBehaviour
 
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
+		gravity = rb2d.gravityScale;
 		sprite = GetComponent<SpriteRenderer> ();
 	}
+
 
 	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
 	void FixedUpdate ()
 	{
+//////////////////////////////////////////////////////
+//Horizontal Movement
+//////////////////////////////////////////////////////
+
 		//Orientation (Determines animation and dashing)
-		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+		if (Input.GetKey (KeyCode.LeftArrow) && canTurn) {
 			lastDir = false;
 			sprite.flipX = true; //Reflect sprite to face left
-		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+		} else if (Input.GetKey (KeyCode.RightArrow) && canTurn) {
 			lastDir = true;
 			sprite.flipX = false;
 		}
-
-		//Movement (Left and Right)
+		//Horizontal Movement
 		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
 		rb2d.velocity = new Vector2 (moveHorizontal * speed, rb2d.velocity.y); //rb2d.velocity.y ensures smooth uninterupted jump
-
-		//Jumping and Wall Jumping
-		if (Input.GetKeyDown (KeyCode.UpArrow)) { //Short hop and short wall jump
+		//Apply drag
+		if (rb2d.velocity.y < terminalVel) {
+			rb2d.AddForce (Vector2.up * drag);
+		}
+//////////////////////////////////////////////////////
+//Jumping and Wall Jumping
+//////////////////////////////////////////////////////
+		 
+		//Short jump/hop and short wall jump
+		if (Input.GetKeyDown (KeyCode.UpArrow)) { //Originally
 			if (onGround) {
 				isJumping = true;
 				jumpTimeCounter = jumpTime; //Reset counter on jump
 				rb2d.AddForce (Vector2.up * jumpHeight, ForceMode2D.Impulse);
-			}
-			//Wall-Jumping
-			if (onLeftWall) {
+			} else if (onLeftWall || onRightWall) {
 				//lastDir = true;
-				rb2d.velocity = Vector2.zero; //Cancel out gravity
-				rb2d.AddForce (Vector2.right * 2 * jumpHeight, ForceMode2D.Impulse); //Bounceback from wall.
-				rb2d.AddForce (Vector2.up * 0.75f * jumpHeight, ForceMode2D.Impulse);
-			} else if (onRightWall) {
-				//lastDir = false;
-				rb2d.velocity = Vector2.zero;
-				rb2d.AddForce (Vector2.left * 2 * jumpHeight, ForceMode2D.Impulse);
-				rb2d.AddForce (Vector2.up * 0.75f * jumpHeight, ForceMode2D.Impulse);
-			}
+				StartCoroutine (WallJump ());
+			} 
 		}
-		if (Input.GetKey (KeyCode.UpArrow) && isJumping) { //Long Jump and Wall-Eject
-			if (jumpTimeCounter > 0) { //Stops when out of jump time
+		//Long Jump
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			if (isJumping && jumpTimeCounter > 0) { //Stops when out of jump time
 				rb2d.AddForce (Vector2.up * jumpHeight, ForceMode2D.Impulse);
 				jumpTimeCounter -= Time.deltaTime; //Timer limits how long one can jump
-			} else {isJumping = false;}
-			//Wall-Jumping
-			if (onLeftWall) {
-				//lastDir = true;
-				rb2d.velocity = Vector2.zero; //Cancel out gravity
-				rb2d.AddForce (Vector2.right * 2 * jumpHeight, ForceMode2D.Impulse); //Bounceback from wall.
-				rb2d.AddForce (Vector2.up * 0.75f * jumpHeight, ForceMode2D.Impulse);
-			} else if (onRightWall) {
-				//lastDir = false;
-				rb2d.velocity = Vector2.zero;
-				rb2d.AddForce (Vector2.left * 2 * jumpHeight, ForceMode2D.Impulse);
-				rb2d.AddForce (Vector2.up * 0.75f * jumpHeight, ForceMode2D.Impulse);
-			}
+			} else
+				isJumping = false; //Stop jump
+			
 		}
-		if(Input.GetKeyUp(KeyCode.UpArrow)){
+		//Check if jumping
+		if (Input.GetKeyUp (KeyCode.UpArrow))  //When key is released, stop jumping immediately
 			isJumping = false;
+		
+
+//////////////////////////////////////////////////////
+//Dashing and Air Dashing
+//////////////////////////////////////////////////////
+
+		if (Input.GetKeyDown (KeyCode.Z) && !isDashing) { //Prevents
+			StartCoroutine (Dash ());
 		}
-		//Dashing (Using Z)
-		if (Input.GetKey (KeyCode.Z)) {
-			if (lastDir) //if facing right
-				rb2d.AddForce (Vector2.right * dashSpeed, ForceMode2D.Impulse);
-			else
-				rb2d.AddForce (Vector2.left * dashSpeed, ForceMode2D.Impulse);
-		}
-	
-		//Blasting
+
+//////////////////////////////////////////////////////
+//Blaster
+//////////////////////////////////////////////////////
+		 
+		//Normal Shot
 		if (Input.GetKeyDown (KeyCode.X)) {
-			if (lastDir) //if facing right
-				Instantiate (lemonR, new Vector2 (transform.position.x + 2, transform.position.y), lemonR.transform.rotation);
+			Vector2 pos;
+			if (lastDir)
+				pos = new Vector2 (transform.position.x + 2, transform.position.y);
 			else
-				Instantiate (lemonL, new Vector2 (transform.position.x - 2, transform.position.y), lemonL.transform.rotation);
+				pos = new Vector2 (transform.position.x - 2, transform.position.y);
+			Instantiate (lemon, pos, lemon.transform.rotation);
 		}
 	}
+
+
+	IEnumerator Dash ()
+	{
+		//Prepare dash
+		canTurn = false;
+		isDashing = true;
+		float time = 0f; //Reset timer
+		speed /= 1000f;	//"Disables" effect of walking temporarily
+		//Air dash
+		if (!onGround) {
+			rb2d.velocity = Vector2.zero;
+			rb2d.gravityScale = 0f; //Anti-gravity if in air
+		}
+		while (time < dashTime) {
+			if (isJumping) { //Enables dash jump
+				time = -0.25f;
+			} 
+			if (lastDir)
+				rb2d.AddForce (Vector2.right * dashSpeed);
+			else
+				rb2d.AddForce (Vector2.left * dashSpeed);
+			time += Time.deltaTime;
+			yield return null; //Advance to next frame
+		}
+		speed *= 1000f; //Re-enables walking
+		//Check where we are
+		if (onLeftWall || onRightWall)
+			rb2d.gravityScale = wallGravity; //Reset gravity (in case of air dash landing)
+		else
+			rb2d.gravityScale = gravity; //Reset gravity (in case of air dash landing)
+
+		canTurn = true;
+		yield return new WaitForSeconds (0.3f); //Delay before second button pressed is allowed
+		isDashing = false;
+	}
+
+	IEnumerator WallJump ()
+	{
+		float time = 0f; //Reset timer
+		speed /= 1000f;	//"Disables" effect of walking temporarily
+		rb2d.velocity = Vector2.zero; //Stop gravity and falling to jump naturally
+
+		while (time < wallJumpTime) {
+			time += Time.deltaTime;
+		
+			if (lastDir) {
+				rb2d.AddForce (Vector2.left * wallJumpKnock);
+				rb2d.AddForce (Vector2.up * wallJumpHeight);
+			} else {
+				rb2d.AddForce (Vector2.right * wallJumpKnock);
+				rb2d.AddForce (Vector2.up * wallJumpHeight);
+			}
+			yield return null; //Advance to next frame
+		}
+		speed *= 1000f;	//"Disables" effect of walking temporarily
+
+	}
+
+
+	//////////////////////////////////////////////////////
+	//OnCollisionEnter
+	//////////////////////////////////////////////////////
 
 	//When on the ground, jumping is allowed
 	void OnCollisionEnter2D (Collision2D other)
@@ -122,30 +194,33 @@ public class CompletePlayerController : MonoBehaviour
 		if (other.gameObject.CompareTag ("LeftWall")) {
 			onLeftWall = true;
 			speed /= 5; //Prevents getting stuck on walls
-			rb2d.gravityScale *= 0.40f; //player descends slower on wall
+			rb2d.gravityScale = wallGravity; //player descends slower on wall
 		}
 		if (other.gameObject.CompareTag ("RightWall")) {
 			onRightWall = true;
 			speed /= 5; //Prevents getting stuck on walls
-			rb2d.gravityScale *= 0.40f; //player descends slower on wall
+			rb2d.gravityScale = wallGravity; //player descends slower on wall
 		}
 	}
+
+	//////////////////////////////////////////////////////
+	//OnCollisionExit
+	//////////////////////////////////////////////////////
 
 	//When jumping, leaves floor and is disallowed from continuous jumping
 	void OnCollisionExit2D (Collision2D other)
 	{
 		if (other.gameObject.CompareTag ("Floor"))
 			onGround = false;
-			
 		if (other.gameObject.CompareTag ("LeftWall")) {
 			onLeftWall = false;
 			speed *= 5; //Prevents getting stuck on walls
-			rb2d.gravityScale /= 0.40f; //player falls normally off of wall
+			rb2d.gravityScale = gravity; //player falls normally off of wall
 		}
 		if (other.gameObject.CompareTag ("RightWall")) {
 			onRightWall = false;
 			speed *= 5; //Prevents getting stuck on walls
-			rb2d.gravityScale /= 0.40f; //player falls normally off of wall
+			rb2d.gravityScale = gravity; //player falls normally off of wall
 		}
 	}
 }
